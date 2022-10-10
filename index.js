bd1661663a4c6085a30f500e9d10263d38cf36da
deviluncle <740128732@qq.com>
2022-10-10 17:01:28 +0800

    database sql
    简单增删改查功能实现

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const koa = require('koa2');
 const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
 const cors = require('koa2-cors');
+const allSqlAction = require('./database') 
 
 const app = new koa();
 app.use(cors());
 app.use(bodyParser());
 
-// app.use(async(ctx) => {
-//     ctx.body = 'hello koa2'
-// })
-
 router.post('/say', ctx => {
+    
     const userName = ctx.request.body.userName
     const password = ctx.request.body.password
     if (userName !== 'crisfei' || password !== 'zkf0701') {
@@ -19,7 +17,9 @@ router.post('/say', ctx => {
         ctx.body = {
             message: '用户名或密码错误!'
         }
+        // ctx.throw(500, '用户名或密码错误!')
     } else {
+        ctx.status = 200
         ctx.body = {
             time: 'dashabi',
             reply: '123'
@@ -27,8 +27,58 @@ router.post('/say', ctx => {
     }
 })
 
+router.post('/test', async (ctx) => {
+    const sql = `SELECT * FROM test_name LIMIT 20`
+    const result = await allSqlAction.allSqlAction(sql);
+    // console.log(result)
+    ctx.body = {
+        data: result
+    }
+})
+
+router.post('/testDelete', async (ctx) => {
+    const id = ctx.request.body.id
+    const sql = `Delete FROM test_name where id = ${id}`
+    await allSqlAction.allSqlAction(sql);
+    // console.log(result)
+    ctx.status = 200
+    ctx.body = {
+        message: '删除成功!'
+    }
+})
+
+router.post('/testEdit', async (ctx) => {
+    const id = ctx.request.body.id
+    const name = ctx.request.body.name
+    const sql = `UPDATE test_name SET name = '${name}' WHERE id = ${id}`
+    await allSqlAction.allSqlAction(sql);
+    // console.log(result)
+    ctx.status = 200
+    ctx.body = {
+        message: '修改成功!'
+    }
+})
+
+
+router.get('/getCsdnArticles', async (ctx) => {
+    const sql = `SELECT * FROM csdn`
+    const result = await allSqlAction.allSqlAction(sql)
+
+    ctx.body = {
+        data: result
+    }
+})
+
+// router.post('/delete', ctx => {
+//   ctx.status = 200
+//   ctx.body = {
+//       message: '删除成功'
+//   }
+// })
+
 // 启动路由
 app.use(router.routes())
+
 
 
 app.listen(3300)
